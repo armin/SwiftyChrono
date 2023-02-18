b8e324b55c7532217063a39d03a183eb0ffe080e
armin <atkroll@gmail.com>
2023-02-18 16:20:20 +1100

    Added a time block meta card. Calculates distance to other time block card in list. Allocated time in block and available time left in block. Also fixed not being able to parse more than one metaword.

--- Package.swift
+++ Package.swift
@@ -1,6 +1,4 @@
 // swift-tools-version:4.2
-
-
 //
 //  Package.swift
 //  SwiftyChrono
@@ -8,14 +6,24 @@
 //  Created by Potix on 2017-01-17.
 //  Copyright © 2017 Potix.
 //
-
-
 import PackageDescription
 
 let package = Package(
     name: "SwiftyChrono",
     products: [
-        .library(name: "SwiftyChrono", targets: ["SwiftyChrono"])
+        .library(
+            name: "SwiftyChrono",
+            targets: ["SwiftyChrono"]),
     ],
-    dependencies: []
+    dependencies: [],
+    targets: [
+        .target(
+            name: "SwiftyChrono",
+            dependencies: [],
+            path: "Sources"),
+        .testTarget(
+            name: "SwiftyChronoTests",
+            dependencies: ["SwiftyChrono"],
+            path: "Tests"),
+    ]
 )
